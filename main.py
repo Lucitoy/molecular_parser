0404051ab33312b8c467398b527920d88cad2284
Lucien C <lucien.casteres@gmail.com>
2021-07-09 11:02:59 +0200

    remove class to match prototype

--- main.py
+++ main.py
@@ -1,75 +1,78 @@
 
 import utils
 
-class FormulaParser(object):
-    bracesStack = []
-    countStacks = [{}]
-    index = 0
 
-    def isClosingChar(self, char):
-        closingBraces = [')', ']', '}']
-        return char in closingBraces and char
 
-    def isOpeningChar(self, char):
-        closingBraces = ['(', '[', '{']
-        return char in closingBraces and char
+bracesStack = []
+countStacks = [{}]
+index = 0
 
-    def areBracesMatching(self, b1, b2):
-        match = {'(': ')', '[': ']', '{': '}'}
-        return isOpeningChar(b1) and match[b1] == b2
+def isClosingChar(char):
+    closingBraces = [')', ']', '}']
+    return char in closingBraces and char
 
-    def getNextAtom(self, molecule, index):
-        # first char should always be an uppercase letter so we check the second one
-        i = index
+def isOpeningChar(char):
+    closingBraces = ['(', '[', '{']
+    return char in closingBraces and char
+
+def areBracesMatching(b1, b2):
+    match = {'(': ')', '[': ']', '{': '}'}
+    return isOpeningChar(b1) and match[b1] == b2
+
+def getNextAtom(molecule, index):
+    # first char should always be an uppercase letter so we check the second one
+    i = index
+    index += 1
+    length = len(molecule)
+    if index < length and molecule[index].islower():
         index += 1
-        length = len(molecule)
-        if index < length and molecule[index].islower():
-            index += 1
 
-        atom = molecule[i: index]
-        number = utils.getNextNumber(molecule, index)
+    atom = molecule[i: index]
+    number = utils.getNextNumber(molecule, index)
 
-        atomLength = len(atom)
-        
-        if number: atomLength += len(number)
-        else: number = 1
+    atomLength = len(atom)
 
-        return {'name': atom, 'count': int(number), 'length': atomLength }
+    if number:
+        atomLength += len(number)
+    else:
+        number = 1
 
-    def parse_molecule(self, molecule):
-        i = 0
-        length = len(molecule)
-        while i < length:
-            if self.isOpeningChar(molecule[i]):
-                i += 1
-                self.countStacks.append({})
+    return {'name': atom, 'count': int(number), 'length': atomLength}
 
-            elif self.isClosingChar(molecule[i]):
-                i += 1
-                number = utils.getNextNumber(molecule, i)
-                lastLevel = self.countStacks.pop()
-                
-                if number: 
-                    i += len(number)
-                    number = int(number)
-                else: number = 1
+def parse_molecule(molecule):
+    i = 0
+    length = len(molecule)
+    while i < length:
+        if isOpeningChar(molecule[i]):
+            i += 1
+            countStacks.append({})
 
-                for atom in lastLevel:
-                    if not atom in self.countStacks[-1]:
-                        self.countStacks[-1][atom] = 0
-                    self.countStacks[-1][atom] += (number * lastLevel[atom])
+        elif isClosingChar(molecule[i]):
+            i += 1
+            number = utils.getNextNumber(molecule, i)
+            lastLevel = countStacks.pop()
 
+            if number:
+                i += len(number)
+                number = int(number)
             else:
-                atom = self.getNextAtom(molecule, i)
-                i += atom['length']
-                if not atom['name'] in self.countStacks[-1]:
-                    self.countStacks[-1][atom['name']] = 0
+                number = 1
 
-                self.countStacks[-1][atom['name']] += atom['count']
+            for atom in lastLevel:
+                if not atom in countStacks[-1]:
+                    countStacks[-1][atom] = 0
+                countStacks[-1][atom] += (number * lastLevel[atom])
+
+        else:
+            atom = getNextAtom(molecule, i)
+            i += atom['length']
+            if not atom['name'] in countStacks[-1]:
+                countStacks[-1][atom['name']] = 0
+
+            countStacks[-1][atom['name']] += atom['count']
+
+    return countStacks[0]
 
 
-        return self.countStacks[0]
 
-
-a = FormulaParser()
-print(a.parse_molecule('K4[ON(SO3)2]2'))
+print(parse_molecule('K4[ON(SO3)2]2'))
